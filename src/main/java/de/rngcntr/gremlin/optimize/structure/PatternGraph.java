3951edaae40072dffafdf892b7ef9a1eab5d2f3d
Florian Grieskamp <florian.grieskamp@gdata.de>
2020-10-14 15:32:33 +0200

    Add license headers
    Signed-off-by: Florian Grieskamp <florian.grieskamp@gdata.de>

--- src/main/java/de/rngcntr/gremlin/optimize/structure/PatternGraph.java
+++ src/main/java/de/rngcntr/gremlin/optimize/structure/PatternGraph.java
@@ -1,10 +1,23 @@
+// Copyright 2020 Florian Grieskamp
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package de.rngcntr.gremlin.optimize.structure;
 
 import de.rngcntr.gremlin.optimize.statistics.StatisticsProvider;
 import de.rngcntr.gremlin.optimize.util.GremlinWriter;
 import de.rngcntr.gremlin.optimize.util.GremlinParser;
 import de.rngcntr.gremlin.optimize.util.Permutations;
-import org.apache.commons.lang.StringUtils;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 
@@ -92,8 +105,7 @@ public class PatternGraph {
 
         Set<Map<PatternElement<?>, PatternElement<?>>> possibleElementMappings = new HashSet<>();
 
-        Permutations.of(otherGraph.elements).forEach(p -> {
-            List<PatternElement<?>> permutation = p.collect(Collectors.toList());
+        Permutations.of(otherGraph.elements).forEach(permutation -> {
             Map<PatternElement<?>, PatternElement<?>> elementMapping = new HashMap<>();
             for (int i = 0; i < elements.size(); ++i) {
                 if (!elements.get(i).equals(permutation.get(i))) return;
--- src/main/java/de/rngcntr/gremlin/optimize/util/Permutations.java
+++ src/main/java/de/rngcntr/gremlin/optimize/util/Permutations.java
@@ -1,34 +1,43 @@
+// Copyright 2020 Florian Grieskamp
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package de.rngcntr.gremlin.optimize.util;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
-/*
-    From: https://gist.github.com/jaycobbcruz/cbabc1eb49f51bfe2ed1db10a06a2b26
+/**
+ * @link https://gist.github.com/jaycobbcruz/cbabc1eb49f51bfe2ed1db10a06a2b26
  */
 public class Permutations {
 
-    public static <T> Stream<Stream<T>> of(final List<T> items) {
-        return IntStream.range(0, factorial(items.size())).mapToObj(i -> permutation(i, items).stream());
+    public static <T> Stream<List<T>> of(final List<T> items) {
+        return LongStream.range(0, factorial(items.size()))
+                .mapToObj(i -> permutation(i, new ArrayList<>(items), new ArrayList<>()));
     }
 
-    private static int factorial(final int num) {
-        return IntStream.rangeClosed(2, num).reduce(1, (x, y) -> x * y);
+    private static long factorial(final long n) {
+        return LongStream.rangeClosed(2, n).reduce(1, (long x, long y) -> x * y);
     }
 
-    private static <T> List<T> permutation(final int count, final LinkedList<T> input, final List<T> output) {
+    private static <T> List<T> permutation(final long i, final List<T> input, final List<T> output) {
         if (input.isEmpty()) { return output; }
 
-        final int factorial = factorial(input.size() - 1);
-        output.add(input.remove(count / factorial));
-        return permutation(count % factorial, input, output);
+        final long factorial = factorial(input.size() - 1);
+        output.add(input.remove((int) (i / factorial)));
+        return permutation(i % factorial, input, output);
     }
-
-    private static <T> List<T> permutation(final int count, final List<T> items) {
-        return permutation(count, new LinkedList<>(items), new ArrayList<>());
-    }
-
 }
